8e2ed9121ff497f6f5b336278c5e980f5a3a4946
zoro-005 <tdevarya@gmail.com>
2025-07-18 13:48:04 +0530

    setting

--- gym_website/settings.py
+++ gym_website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', '.render.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.render.com', 'fitness-syntax-error.onrender.com']
 
 
 # Application definition
